d30c705999aa07d55bcdda7b55b0d4e78e9dbb81
brahim ayad <brahim-ayad@hotmail.com>
2020-05-04 15:45:56 -0700

    securtiy adjusting

--- models/models.py
+++ models/models.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 
- from odoo import models, fields, api
+from odoo import models, fields
 
 
- class fishing(models.Model):
+class fishing(models.Model):
+ 
      _name = 'fishing.list'
 #     _description = 'fishing.fishing'
 
      name = fields.Char()
-     Number = fields.Integer()
+     number = fields.Integer()
 #     value2 = fields.Float(compute="_value_pc", store=True)
      description = fields.Text()
 #
